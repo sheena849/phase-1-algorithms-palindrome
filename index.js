b940e510369fa5d2afffb1bd0ac9610cb58abecc
sheena849 <sheenamugo04@gmail.com>
2024-10-28 09:07:46 +0300

    Algorithims Palindrome

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  // Write your algorithm here => Word that is the same when read from either left or right
+  // convert each character of a word to lowerCase
+  const newWord = word.toLowerCase();
+  // Initialise the pointers, one from the beginning and the other from the end of the word
+  let left = 0;
+  let right = newWord.length - 1;
+  // Loop untill the two pointers are equal(meeting)
+  while (left < right) {
+    // If the characters are not equal, return false
+    if (newWord[left]!== newWord[right]) {
+      return false;
+    }
+
+    left++;
+    right--;
+  
+}
+  // Return true if the word is a palindrome
+  return true;
 }
 
 /* 
